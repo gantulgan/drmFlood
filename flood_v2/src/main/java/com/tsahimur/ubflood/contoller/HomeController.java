315247bfceede4d78fbaf0b3fa3ab7536c313396
gantulga <gantulga.nadmid@gmail.com>
2015-01-09 23:45:27 +0800

    merge Home Controller

--- flood_v2/src/main/java/com/tsahimur/ubflood/contoller/HomeController.java
+++ flood_v2/src/main/java/com/tsahimur/ubflood/contoller/HomeController.java
@@ -43,14 +43,9 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		List<Category> categories = categoryService.getRootCategories();
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("categories", categories);
 		return "home";
 	}
 	
@@ -72,6 +67,11 @@ public class HomeController {
 		return "mapper";
 	}
 	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+		return "admin/login";
+	}
+	
 	// ---------------------- post list & view
 	@RequestMapping(value = "/content&{id}", method = RequestMethod.GET)
 	public String user_getPosts(Model model) {
@@ -91,11 +91,4 @@ public class HomeController {
 		model.addAttribute( "post", post );
 		return Constant.PAGE.USER_VIEW_POST;
 	}
-	
-	
-	@RequestMapping(value="dbtest", method = RequestMethod.GET)
-	public String testDB(){
-		return "/admin/checkDB";
-	}
-	
 }
